acc37452a7155c6434a2a1265a0bd38a03fddec6
dariorojas6 <dario43055@gmail.com>
2023-04-17 20:12:24 -0300

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,12 @@
+export const data = [
+    {
+        symbol: "😴",
+        name: "cara con sueño",
+        keywords: "dream,exhausted, tired "
+    },
+    {
+        symbol: "❤",
+        name: "corazón",
+        keywords: "heart, love, red  "
+    }
+]

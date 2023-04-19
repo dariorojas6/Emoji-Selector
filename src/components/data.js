d02a38840158fee0fc6d81380e57c6745ad11afe
dariorojas6 <dario43055@gmail.com>
2023-04-18 21:13:21 -0300

    componentes emojiPicker y emojiPickerInput

--- src/components/data.js
+++ src/components/data.js
@@ -2,11 +2,41 @@ export const data = [
     {
         symbol: "😴",
         name: "cara con sueño",
-        keywords: "dream,exhausted, tired "
+        keywords: "dream exhausted tired "
     },
     {
         symbol: "❤",
         name: "corazón",
-        keywords: "heart, love, red  "
+        keywords: "heart love red  "
+    },
+    {
+        symbol: "😀",
+        name: "cara feliz",
+        keywords: "happy smile happiness"
+    },
+    {
+        symbol: "😂",
+        name: "cara llorando",
+        keywords: "laughter fun crying"
+    }
+    ,{
+        symbol: "🤑",
+        name: "cara con lengua de dinero",
+        keywords: "language money dollar"
+    }
+    ,{
+        symbol: "😎",
+        name: "cara sonriendo con lentes",
+        keywords: "smile glasses"
+    },
+    {
+        symbol: "😭",
+        name: "cara llorando fuerte",
+        keywords: "cry sad tears"
+    },
+    {
+        symbol: "😠",
+        name: "cara de enojado",
+        keywords: "angry rage fight"
     }
 ]

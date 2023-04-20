9302ca0f255fae5e69e47b6e11f5ddc823758d98
dariorojas6 <dario43055@gmail.com>
2023-04-19 21:42:09 -0300

    componente EmojiSearch

--- src/components/emojiPicker/emojiPicker.jsx
+++ src/components/emojiPicker/emojiPicker.jsx
@@ -1,5 +1,48 @@
-export default function EmojiPicker(){
-return <div>
-    Hola
-</div>
+import { forwardRef, useState } from "react";
+
+import { data as emojiList } from "./data.js"
+import EmojiSearch from "./emojiSearch.jsx";
+
+export function EmojiPicker(props, inputRef) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [emojis, setEmojis] = useState(emojiList);
+
+    function handleClickOpen() {
+        setIsOpen(!isOpen);
+    }
+
+    function handleSearch(e) {
+        const q = e.target.value.toLowerCase();
+
+        if (!!q) {
+            const search = emojiList.filter(emoji => {
+                return (
+                    emoji.name.toLowerCase().includes(q) ||
+                    emoji.keywords.toLowerCase().includes(q)
+                );
+            });
+            setEmojis(search)
+        }else{
+            setEmojis(emojiList)
+        }
+    }
+
+    function EmojiPickerContainer() {
+        return <div>
+            <EmojiSearch onSearch={handleSearch} />
+            <div>
+                {emojiList.map((emoji) => (
+                    <div>{emoji.symbol}</div>
+                ))}
+            </div>
+        </div>
+    }
+    return (
+        <div>
+            <button onClick={handleClickOpen}>😨</button>
+            {isOpen ? <EmojiPickerContainer /> : ""}
+        </div>
+    )
 }
+
+export default forwardRef(EmojiPicker)

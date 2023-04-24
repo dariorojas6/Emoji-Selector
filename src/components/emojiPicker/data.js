6469e388747753b910cf38a52d1d6359c4a51adb
dariorojas6 <dario43055@gmail.com>
2023-04-24 20:57:05 -0300

    agregacion de emojis

--- src/components/emojiPicker/data.js
+++ src/components/emojiPicker/data.js
@@ -1,42 +1,73 @@
 export const data = [
     {
-        symbol: "😴",
-        name: "cara con sueño",
-        keywords: "dream exhausted tired sleep "
-    },
-    {
-        symbol: "❤",
-        name: "corazón",
-        keywords: "heart love red  "
-    },
-    {
         symbol: "😀",
         name: "cara feliz",
         keywords: "happy smile happiness"
     },
     {
+        symbol: "😁",
+        name: "cara y ojos sonrientes",
+        keywords: "smile happy smiling"
+    },
+    {
         symbol: "😂",
         name: "cara llorando",
         keywords: "laughter fun crying"
+    },
+    {
+        symbol: "🤣",
+        name: "cara llorando de risa",
+        keywords: "laughter crying "
+    },
+    {
+        symbol: "😃",
+        name: "sonrisa con boca abierta",
+        keywords: "smile mouth "
+    },
+    {
+        symbol: "😄",
+        name: "boca y ojos sonrientes",
+        keywords: "smile eyes "
+    },
+    
+    , {
+        symbol: "😅",
+        name: "sonrisa y sudor frio",
+        keywords: "smile sweat cold"
     }
-    ,{
-        symbol: "🤑",
-        name: "cara con lengua de dinero",
-        keywords: "language money dollar"
-    }
-    ,{
+    , {
+        symbol: "😆",
+        name: "sonrisa con boca abierta y ojos cerrados",
+        keywords: "smile eyes closed "
+    },
+    {
+        symbol: "😉",
+        name: "cara con guiño",
+        keywords: "face wink"
+    },
+    {
+        symbol: "😊",
+        name: "cara alegre",
+        keywords: "face cheerful smile"
+    },
+    {
+        symbol: "😋",
+        name: "cara de broma",
+        keywords: "face joke language"
+    },
+    {
         symbol: "😎",
-        name: "cara sonriendo con lentes",
-        keywords: "smile glasses"
+        name: "cara con anteojos",
+        keywords: "face  glasses smile"
     },
     {
-        symbol: "😭",
-        name: "cara llorando fuerte",
-        keywords: "cry sad tears"
+        symbol: "😍",
+        name: "cara con ojos de corazon",
+        keywords: "face eyes heart"
     },
     {
-        symbol: "😠",
-        name: "cara de enojado",
-        keywords: "angry rage fight"
-    }
+        symbol: "😘",
+        name: "cara lanzando un beso",
+        keywords: "face kiss"
+    },
 ]
